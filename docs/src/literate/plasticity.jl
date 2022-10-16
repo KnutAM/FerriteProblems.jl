d669533f6781d3e7fb680b9adc380ac417a625ca
Knut Andreas Meyer <knutam@gmail.com>
2022-10-16 19:35:48 +0200

    Update plasticity example

--- docs/src/literate/plasticity.jl
+++ docs/src/literate/plasticity.jl
@@ -17,7 +17,7 @@ include("J2Plasticity.jl");
 # ## Problem definition
 # We first create the problem's definition
 
-traction_function(t) = t*1.e7   ## N/m²
+traction_function(time) = time*1.e7 # N/m²
 
 function setup_problem_definition()
     ## Define material properties
@@ -25,7 +25,7 @@ function setup_problem_definition()
     material = J2Plasticity(E, 0.3, 200.e6, E/20)
     
     ## Mesh
-    L = 10.0; w = 1.0; h = 1.0  ## Dimensions
+    L = 10.0; w = 1.0; h = 1.0  # Dimensions
     n = 2
     nels = (10n, n, 2n)
     P1 = Vec((0.0, 0.0, 0.0))
@@ -64,12 +64,13 @@ function setup_problem_definition()
     
 
     return FEDefinition(dh, ch, nh, cv, material, states)
-end
+end;
 
 # For the problem at hand, we need to define the element routine, 
 # following `FerriteAssembly`s interface. This function is almost equivalent to 
-# the `assemble_cell!` in the original example, but we don't have to reinit! the 
-# cellvalues as this has already been done. 
+# the `assemble_cell!` in the original example, except that 
+# 1) We don't have to `reinit!` as `FerriteAssembly` does that before calling
+# 2) The traction is handled by `FerriteNeumann` and is not done for each cell
 
 function FerriteAssembly.element_routine!(
     Ke::AbstractMatrix, re::AbstractVector, 
@@ -86,15 +87,15 @@ function FerriteAssembly.element_routine!(
         dΩ = getdetJdV(cellvalues, q_point)
         for i in 1:n_basefuncs
             δϵ = shape_symmetric_gradient(cellvalues, q_point, i)
-            re[i] += (δϵ ⊡ σ) * dΩ ## add internal force to residual
-            for j in 1:i ## loop only over lower half
+            re[i] += (δϵ ⊡ σ) * dΩ # add internal force to residual
+            for j in 1:i # loop only over lower half
                 Δϵ = shape_symmetric_gradient(cellvalues, q_point, j)
                 Ke[i, j] += δϵ ⊡ D ⊡ Δϵ * dΩ
             end
         end
     end
     symmetrize_lower!(Ke)
-end
+end;
 
 function symmetrize_lower!(K)
     for i in 1:size(K,1)
@@ -102,46 +103,44 @@ function symmetrize_lower!(K)
             K[i,j] = K[j,i]
         end
     end
-end
+end;
 
-# At this point, we are now ready to define a problem that can be solved 
-# as `problem = FerriteProblem(setup_problem_definition())`. 
-# However, if we want to have any results, apart from the final state,
-# we need to define postprocessing tasks. In contrast to the original example, 
+# At this point, we can define 
+# `problem = FerriteProblem(setup_problem_definition())`, which can be solved 
+# with `FESolvers.jl`'s `solve_problem!`. 
+# But to get any results apart from the final state, we need to define 
+# the postprocessing after each step.
+
+# ## Setup postprocessing
+# In contrast to the original example, 
 # we do not save directly to a vtk-file, but use `FerriteProblem`'s IO features 
 # to save to JLD2 files. This has the advantage that further postprocessing can 
 # be done after the simulation, and we can then choose to export to the VTK-format 
 # or plot directly using e.g. `FerriteViz.jl`. 
-
-# ## Setup postprocessing
 # We start by defining our custom postprocessing type.
 struct PlasticityPostProcess{T}
     tmag::Vector{T}
     umag::Vector{T}
 end
-PlasticityPostProcess() = PlasticityPostProcess(Float64[], Float64[])
+PlasticityPostProcess() = PlasticityPostProcess(Float64[], Float64[]);
 
 # With this postprocessing type, we can now define the postprocessing in FESolvers.
 # Since the `FerriteProblem` type is used for all types of problems, it can be 
 # useful to dispatch on the contained postprocessing type to have multiple problems 
 # defined in the same session/package. 
 function FESolvers.postprocess!(p::FerriteProblem{<:PlasticityPostProcess}, step, solver)
-    ## First, we do the simple postprocessing: Saving variables directly in the struct 
+    ## First, we save some values directly in the `post` struct 
     push!(p.post.tmag, traction_function(FerriteProblems.gettime(p)))
     push!(p.post.umag, maximum(abs, FESolvers.getunknowns(p)))
 
-    ## Second, we save the results to our results in each time step to file
+    ## Second, we save some results to file
     ## * We must always start by adding the next step. 
     FerriteProblems.addstep!(p.io, FerriteProblems.gettime(p))
     ## * Save the dof values (only displacments in this case)
     FerriteProblems.savedofdata!(p.io, FESolvers.getunknowns(p))
     ## * Save the state in each integration point
     FerriteProblems.saveipdata!(p.io, FerriteProblems.getstate(p), "state")
-    ## * We could save the maximum displacements and traction as
-    ## FerriteProblems.saveglobaldata!(p.io, last(p.post.umag), "umag")
-    ## FerriteProblems.saveglobaldata!(p.io, last(p.post.tmag), "tmag")
-    ## But it is not necessary as the `PlasticityPostProcess` struct is also saved
-end
+end;
 
 # ## Solving the problem
 # First, we define a helper function to plot the results after the solution
@@ -155,16 +154,17 @@ function plot_results(problem::FerriteProblem{<:PlasticityPostProcess}; plt=plot
     return plt
 end;
 
-# Wrap solver to make it save any open stuff at the end, 
-# also in case of an exception 
-# This is temporary, would be nice to handle this in FESolvers
+# **Temporary:** 
+# Wrap `solve_problem!` to make it save any open stuff at the end, 
+# even in case of an exception.
+# This is temporary, but would be nice to handle within FESolvers
 function wrapped_solve!(solver, problem)
     try
         solve_problem!(solver, problem)
     finally
         FerriteProblems.close_problem(problem)
     end
-end
+end;
 
 # Finally, we can solve the problem with different time stepping strategies 
 # and plot the results
@@ -197,9 +197,11 @@ function example_solution()
 end;
 
 plt, problem, solver = example_solution();
-display(plt)
+display(plt);
 
-# Which gives the following result 
+# Which gives the following result
+# 
+# 
 # ![](plasticity.svg)
 
 #md # ## Plain program
