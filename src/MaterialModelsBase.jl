9b7f14e15f5028cf98461a0c5922778e83aec07a
Knut Andreas Meyer <knutam@gmail.com>
2022-10-23 12:51:16 +0200

    Add element definition for MaterialModelsBase.jl

---
+++ src/MaterialModelsBase.jl
@@ -0,0 +1,27 @@
+# Mechanical materials that are defined according to the `MaterialModelsBase.jl`
+# have already built-in support for convenience
+get_material_cache(m::AbstractMaterial) = get_cache(m)
+
+function FerriteAssembly.element_routine!(
+    Ke::AbstractMatrix, re::AbstractVector, state::Vector{<:AbstractMaterialState},
+    ae::AbstractVector, material::AbstractMaterial, cellvalues::CellVectorValues, 
+    dh_fh, Δt, buffer::CellBuffer)
+    cache = buffer.cache
+    n_basefuncs = getnbasefunctions(cellvalues)
+    for q_point in 1:getnquadpoints(cellvalues)
+        # For each integration point, compute stress and material stiffness
+        ϵ = function_symmetric_gradient(cellvalues, q_point, ae) # Total strain
+        σ, D, state[q_point] = material_response(material, ϵ, state[q_point], Δt, cache)
+
+        dΩ = getdetJdV(cellvalues, q_point)
+        for i in 1:n_basefuncs
+            ∇δN = shape_symmetric_gradient(cellvalues, q_point, i)
+            re[i] += (∇δN ⊡ σ) * dΩ # add internal force to residual
+            ∇δN_D = ∇δN ⊡ D         # temporary value for speed
+            for j in 1:n_basefuncs
+                ∇N = shape_symmetric_gradient(cellvalues, q_point, j)
+                Ke[i, j] += (∇δN_D ⊡ ∇N) * dΩ
+            end
+        end
+    end
+end;
