cc3ebfd563d65225f5291c645fee8a204ab4750a
Knut Andreas Meyer <knutam@gmail.com>
2023-02-03 21:10:18 +0100

    Improve detection of Ferrite PR427 to avoid redefinitions

--- src/FerritePRs/include_prs.jl
+++ src/FerritePRs/include_prs.jl
@@ -2,7 +2,10 @@
 # Once merged, they should be included via a `@static` construct if the Ferrite version is old.
 
 include("PR457.jl") # global_dof_range
-include("PR427.jl") # add!(::ConstraintHandler, ::Dirichlet) for nonconcrete celltypes
+
+@static if !(isdefined(Ferrite, :overlaps) || isdefined(Ferrite, :_all_or_some_in_cellset))
+    include("PR427.jl") # add!(::ConstraintHandler, ::Dirichlet) for nonconcrete celltypes
+end
 
 @static if !isdefined(Ferrite, :apply_analytical!)
     include("PR532.jl")
