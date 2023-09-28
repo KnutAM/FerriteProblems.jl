84b7c6b9ccaff0a35530d6f3f3591aa91477b324
Knut Andreas Meyer <knutam@gmail.com>
2023-09-28 19:54:15 +0200

    Fix tests

--- test/CustomStiffness.jl
+++ test/CustomStiffness.jl
@@ -28,35 +28,35 @@ end
     cv = CellScalarValues(QuadratureRule{2,RefCube}(2), ip)
     mw = CSTestWrap(FerriteAssembly.ExampleElements.StationaryFourier(1.0))
     m = CustomStiffness(mw, :positive)
-    buffer, new_states = setup_assembly(dh, m, cv)
-    buffer_ad, new_states = setup_assembly(dh, m, cv; autodiffbuffer=true)
+    buffer = setup_domainbuffer(DomainSpec(dh, m, cv))
+    buffer_ad = setup_domainbuffer(DomainSpec(dh, m, cv); autodiffbuffer=true)
     K = create_sparsity_pattern(dh)
     r = zeros(ndofs(dh))
     a = rand(ndofs(dh))
 
     assembler = start_assemble(K, r)
-    doassemble!(assembler, new_states, buffer)
+    work!(assembler, buffer)
     d1 = sum(diag(collect(K)))
     m.stiffness_type = :negative
     assembler = start_assemble(K, r)
-    doassemble!(assembler, new_states, buffer)
+    work!(assembler, buffer)
     d2 = sum(diag(collect(K)))
     @test d1 ≈ -d2
 
     assembler = start_assemble(K, r)
-    doassemble!(assembler, new_states, buffer_ad; a=a)
+    work!(assembler, buffer_ad; a=a)
     @test d2 ≈ sum(diag(collect(K)))
 
     r2 = rand(length(r))
     r_assembler = ReAssembler(r2)
-    doassemble!(r_assembler, new_states, buffer; a=a)
+    work!(r_assembler, buffer; a=a)
     @test r ≈ r2
 
     # Check that correct error is thrown if not implemented
     m_throw = CustomStiffness(mw.m, :positive) # element_routine! not implemented 
-    b_throw, _ = setup_assembly(dh, m_throw, cv)
+    b_throw = setup_domainbuffer(DomainSpec(dh, m_throw, cv))
     assembler = start_assemble(K, r)
-    @test_throws ArgumentError doassemble!(assembler, new_states, b_throw)
+    @test_throws ArgumentError work!(assembler, b_throw)
     msg = "You must implement element_routine!, normally for m::CustomStiffness{<:"
-    @test_throws msg doassemble!(assembler, new_states, b_throw)
+    @test_throws msg work!(assembler, b_throw)
 end
