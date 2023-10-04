6ef01d882b704c3ed7f8cf4b6304da540fb891d6
Knut Andreas Meyer <knutam@gmail.com>
2023-10-04 18:15:12 +0200

    Fix multiple domains construction

--- src/FEDefinition.jl
+++ src/FEDefinition.jl
@@ -33,7 +33,7 @@ struct FEDefinition{DH,CH,LH,CC}
 end
 _extract_dofhandler(domain::DomainSpec) = domain.sdh.dh
 _extract_dofhandler(domains::Dict{String, DomainSpec}) = first(values(domains)).sdh.dh
-function FEDefinition(domain::Union{DomainSpec, Dict{String, DomainSpec}}; 
+function FEDefinition(domain::Union{DomainSpec, Dict{String, <:DomainSpec}}; 
         ch, lh=LoadHandler(_extract_dofhandler(domain)),
         convergence_criterion=AbsoluteResidual(), initial_conditions=NamedTuple(), 
         )
